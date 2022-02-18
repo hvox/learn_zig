11f75166b3a940cd65b81c65bb9a0a51f11455eb
Uladzislau Khamkou <kham@tuta.io>
2022-02-18 23:08:17 +0300

    name threads using argument passing

--- threads.zig
+++ threads.zig
@@ -7,18 +7,19 @@ threadlocal var local_counter: i32 = 0;
 var global_counter: i32 = 0;
 
 pub fn main() !void {
-    const thread1 = try std.Thread.spawn(.{}, print_counters, .{});
-    const thread2 = try std.Thread.spawn(.{}, print_counters, .{});
-    const thread3 = try std.Thread.spawn(.{}, print_counters, .{});
-    print_counters();
+    const thread1 = try std.Thread.spawn(.{}, print_counters, .{1});
+    const thread2 = try std.Thread.spawn(.{}, print_counters, .{2});
+    const thread3 = try std.Thread.spawn(.{}, print_counters, .{3});
+    print_counters(4);
     thread1.join();
     thread2.join();
     thread3.join();
 }
 
-fn print_counters() void {
+fn print_counters(id: i32) void {
     local_counter += 1;
     global_counter += 1;
+    print("thread #{}\n", .{id});
     print("local counter: {}\n", .{local_counter});
     print("global counter: {}\n", .{global_counter});
 }
