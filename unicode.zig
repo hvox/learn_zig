a880d690639cdced29126dae02debf9c0390be3a
Uladzislau Khamkou <kham@tuta.io>
2022-02-18 21:52:41 +0300

    create demo of strings

---
+++ unicode.zig
@@ -0,0 +1,9 @@
+// zig 0.10.0
+
+const print = @import("std").debug.print;
+
+pub fn main() void {
+    const msg = "2∫π = π^2";
+    print("{s}  : {s}\n", .{ msg, @typeName(@TypeOf(msg)) });
+    print("msg[2] = {d}:{s}\n", .{ msg[2], @typeName(@TypeOf(msg[2])) });
+}
