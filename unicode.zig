e3865bced0c5b1fc8750511ee7943120ce5433f1
Uladzislau Khamkou <kham@tuta.io>
2022-02-18 22:10:16 +0300

    use "²" instead of "^2" in unicode.zig

--- unicode.zig
+++ unicode.zig
@@ -3,7 +3,7 @@
 const print = @import("std").debug.print;
 
 pub fn main() void {
-    const msg = "2∫π = π^2";
-    print("{s}  : {s}\n", .{ msg, @typeName(@TypeOf(msg)) });
+    const msg = "2∫π = π²";
+    print("{s}  :  {s}\n", .{ msg, @typeName(@TypeOf(msg)) });
     print("msg[2] = {d}:{s}\n", .{ msg[2], @typeName(@TypeOf(msg[2])) });
 }
