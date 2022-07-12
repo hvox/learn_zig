a9ad8061a33c39ddfc76723b84b99974c6cff828
Uladzislau Khamkou <kham@tuta.io>
2022-07-12 10:58:27 +0300

    fix arrays2.zig

--- ziglings/005_arrays2.zig
+++ ziglings/005_arrays2.zig
@@ -21,26 +21,13 @@ const std = @import("std");
 pub fn main() void {
     const le = [_]u8{ 1, 3 };
     const et = [_]u8{ 3, 7 };
+    const leet = le ++ et;
+    const bit_pattern = [_]u8{ 1, 0, 0, 1 } ** 3;
 
-    // (Problem 1)
-    // Please set this array concatenating the two arrays above.
-    // It should result in: 1 3 3 7
-    const leet = ???;
-
-    // (Problem 2)
-    // Please set this array using repetition.
-    // It should result in: 1 0 0 1 1 0 0 1 1 0 0 1
-    const bit_pattern = [_]u8{ ??? } ** 3;
-
-    // Okay, that's all of the problems. Let's see the results.
-    //
     // We could print these arrays with leet[0], leet[1],...but let's
     // have a little preview of Zig 'for' loops instead:
     //
     //    for (<item array>) |item| { <do something with item> }
-    //
-    // Don't worry, we'll cover looping properly in upcoming
-    // lessons.
     //
     std.debug.print("LEET: ", .{});
 
