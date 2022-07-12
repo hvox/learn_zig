c1c42a35b59039b0e2a67b661e42a9e216bf79de
Uladzislau Khamkou <kham@tuta.io>
2022-07-12 12:33:14 +0300

    fix for2.zig

--- ziglings/016_for2.zig
+++ ziglings/016_for2.zig
@@ -22,9 +22,7 @@ pub fn main() void {
 
     // Now we'll convert the binary bits to a number value by adding
     // the value of the place as a power of two for each bit.
-    //
-    // See if you can figure out the missing piece:
-    for (bits) |bit, ???| {
+    for (bits) |bit, i| {
         // Note that we convert the usize i to a u32 with
         // @intCast(), a builtin function just like @import().
         // We'll learn about these properly in a later exercise.
