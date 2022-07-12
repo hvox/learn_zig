a57ae8583cdeed81edc64d10c241beb7a85a4112
Uladzislau Khamkou <kham@tuta.io>
2022-07-12 11:08:17 +0300

    fix strings.zig

--- ziglings/006_strings.zig
+++ ziglings/006_strings.zig
@@ -20,24 +20,13 @@ const std = @import("std");
 
 pub fn main() void {
     const ziggy = "stardust";
-
-    // (Problem 1)
-    // Use array square bracket syntax to get the letter 'd' from
-    // the string "stardust" above.
-    const d: u8 = ziggy[???];
-
-    // (Problem 2)
-    // Use the array repeat '**' operator to make "ha ha ha ".
-    const laugh = "ha " ???;
-
-    // (Problem 3)
-    // Use the array concatenation '++' operator to make "Major Tom".
-    // (You'll need to add a space as well!)
+    const d: u8 = ziggy[4];
+    const laugh = "ha " ** 3;
     const major = "Major";
     const tom = "Tom";
-    const major_tom = major ??? tom;
+    const major_tom = major ++ " " ++ tom;
 
-    // That's all the problems. Let's see our results:
+    // Let's see our results:
     std.debug.print("d={u} {s}{s}\n", .{ d, laugh, major_tom });
     // Keen eyes will notice that we've put 'u' and 's' inside the '{}'
     // placeholders in the format string above. This tells the
