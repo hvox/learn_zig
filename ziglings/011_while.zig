5620a885558d6f74066a24ec6405ca87905ed0c8
Uladzislau Khamkou <kham@tuta.io>
2022-07-12 11:40:49 +0300

    fix while.zig

--- ziglings/011_while.zig
+++ ziglings/011_while.zig
@@ -20,8 +20,8 @@ const std = @import("std");
 pub fn main() void {
     var n: u32 = 2;
 
-    // Please use a condition that is true UNTIL "n" reaches 1024:
-    while (???) {
+    // Condition that is true UNTIL "n" reaches 1024:
+    while (n < 1024) {
         // Print the current number
         std.debug.print("{} ", .{n});
 
@@ -29,6 +29,6 @@ pub fn main() void {
         n *= 2;
     }
 
-    // Once the above is correct, this will print "n=1024"
+    // This will print "n=1024"
     std.debug.print("n={}\n", .{n});
 }
