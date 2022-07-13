058a6f70945bc09aa986ad5aeb2be57fa03040cf
Uladzislau Khamkou <kham@tuta.io>
2022-07-13 21:47:30 +0300

    fix errors2.zig

--- ziglings/022_errors2.zig
+++ ziglings/022_errors2.zig
@@ -19,10 +19,10 @@ const std = @import("std");
 const MyNumberError = error{TooSmall};
 
 pub fn main() void {
-    var my_number: ??? = 5;
+    var my_number: MyNumberError!i8 = 5;
 
     // Looks like my_number will need to either store a number OR
-    // an error. Can you set the type correctly above?
+    // an error.
     my_number = MyNumberError.TooSmall;
 
     std.debug.print("I compiled!\n", .{});
