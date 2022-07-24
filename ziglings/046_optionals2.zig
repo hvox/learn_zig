faef28438d5be61b54cca5ded0cf4d35e08a665b
Uladzislau Khamkou <kham@tuta.io>
2022-07-24 18:55:23 +0300

    fix optionals2.zig

--- ziglings/046_optionals2.zig
+++ ziglings/046_optionals2.zig
@@ -21,7 +21,7 @@ const std = @import("std");
 
 const Elephant = struct {
     letter: u8,
-    tail: *Elephant = null, // Hmm... tail needs something...
+    tail: ?*Elephant = null,
     visited: bool = false,
 };
 
@@ -49,9 +49,8 @@ fn visitElephants(first_elephant: *Elephant) void {
         e.visited = true;
 
         // We should stop once we encounter a tail that
-        // does NOT point to another element. What can
-        // we put here to make that happen?
-        if (e.tail == null) ???;
+        // does NOT point to another element.
+        if (e.tail == null) break;
 
         e = e.tail.?;
     }
