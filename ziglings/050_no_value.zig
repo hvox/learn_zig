d6a43c7ac03324a7777b9b316f6ea000d3402cdd
Uladzislau Khamkou <kham@tuta.io>
2022-07-26 12:00:00 +0300

    fix no_value.zig

--- ziglings/050_no_value.zig
+++ ziglings/050_no_value.zig
@@ -57,18 +57,17 @@
 //   * errors    - there is no value because something went wrong
 //   * void      - there will NEVER be a value stored here
 //
-// Please use the correct "no value" for each ??? to make this program
-// print out a cursed quote from the Necronomicon. ...If you dare.
+// Print out a cursed quote from the Necronomicon. ...If you dare.
 //
 const std = @import("std");
 
 const Err = error{Cthulhu};
 
 pub fn main() void {
-    var first_line1: *const [16]u8 = ???;
+    var first_line1: *const [16]u8 = undefined;
     first_line1 = "That is not dead";
 
-    var first_line2: Err!*const [21]u8 = ???;
+    var first_line2: Err!*const [21]u8 = undefined;
     first_line2 = "which can eternal lie";
 
     std.debug.print("{s} {s} / ", .{ first_line1, first_line2 });
@@ -76,8 +75,8 @@ pub fn main() void {
     printSecondLine();
 }
 
-fn printSecondLine() ??? {
-    var second_line2: ?*const [18]u8 = ???;
+fn printSecondLine() void {
+    var second_line2: ?*const [18]u8 = null;
     second_line2 = "even death may die";
 
     std.debug.print("And with strange aeons {s}.\n", .{second_line2.?});
