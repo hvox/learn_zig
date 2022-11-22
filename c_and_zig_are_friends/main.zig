cc771305bc31a741d31224a93cc1563438f43b72
Uladzislau Khamkou <kham@tuta.io>
2022-11-22 16:52:08 +0300

    fix example of usage of C functions from zig

--- c_and_zig_are_friends/main.zig
+++ c_and_zig_are_friends/main.zig
@@ -9,9 +9,9 @@ const c_code = @cImport({
 pub fn main() !void {
     var a: c_int = -25;
     var b: c_int = 333;
-    print("We have successfully imported funcion {}!\n", .{c_code.add});
+    print("We have successfully imported funcion {s}!\n", .{"c_code.add"});
     print("Btw, {} + {} = {} mod 257\n", .{ a, b, c_code.add(a, b) });
-    print("Also we have imported funcion {}!\n", .{c_code.hi});
+    print("Also we have imported funcion {s}!\n", .{"c_code.hi"});
     print("Let's call it...\n", .{});
     c_code.hi();
 }
